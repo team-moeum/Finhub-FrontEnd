dfcf987a195b30c00c6135fad2aa9fea368103fd
widse <wngud6012@naver.com>
2024-01-28 20:19:58 +0900

    Init(Feat) : Add Not Found UI #5

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import LinkButton from '@/components/Button/LinkButton';
+import styled from '@emotion/styled';
+ 
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
+`
+
+export default function NotFound() {
+  return (
+    <Container>
+      <p style={{fontSize:'1.5em', fontWeight:'700'}}>Not Found</p>
+      <p>Could not find requested resource</p>
+      <LinkButton tag="button" type="contained" href="/" width={150} height={40} ripple animate>Return Home</LinkButton>
+    </Container>
+  )
+}
