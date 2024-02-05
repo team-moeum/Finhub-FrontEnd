0a7494be795f1d2c111c9b908fcdbee338b3ccd4
widse <wngud6012@naver.com>
2024-02-06 01:50:54 +0900

    Init(UI dev): Main_1

--- src/app/_component/Catergory/CategoryCard.tsx
+++ src/app/_component/Catergory/CategoryCard.tsx
@@ -2,39 +2,80 @@
 
 import { Pagination, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import PigIcon from '@/assets/Pig.svg';
+import Image from 'next/image';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import style from "./CategoryCard.module.css";
+import "./CategoryCard.css";
 
 export default function CategoryCard() {
     return (
         <div>
              <Swiper
-                className={style.cg_card_swiper}
+                className="cg_card_swiper"
                 modules={[Pagination, Autoplay]}
                 spaceBetween={50}
                 slidesPerView={1}
                 //autoplay = {{ delay: 3000, disableOnIntercation: false}}
                 //loop={true}
-                pagination={{ clickable: true }}
+                pagination={{ clickable: true}}
                 onSwiper={(swiper : any) => console.log(swiper)}
                 onSlideChange={() => console.log('slide change')}
             >
                 <SwiperSlide>
-                    <div className={style.card}>
-                        
+                    <div className="card">
+                        <div className="img-box">
+                            <Image
+                                src={PigIcon}
+                                alt={'pig-icon'}
+                                width={80}
+                                height={64}
+                            />
+                        </div>
+                        <div className="text-container">
+                            <div className="text-box">
+                                <p className="title">초보자도 주식 할 수 있다!</p>
+                                <p className="content">주식을 처음할 때 꼭 알아야 하는<br />기초 지식 7가지</p>
+                            </div>
+                        </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={style.card}>
-                        
+                    <div className="card">
+                        <div className="img-box">
+                            <Image
+                                src={PigIcon}
+                                alt={'pig-icon'}
+                                width={80}
+                                height={64}
+                            />
+                        </div>
+                        <div className="text-container">
+                            <div className="text-box">
+                                <p className="title">초보자도 주식 할 수 있다!</p>
+                                <p className="content">주식을 처음할 때 꼭 알아야 하는<br />기초 지식 7가지</p>
+                            </div>
+                        </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={style.card}>
-                        
+                     <div className="card">
+                        <div className="img-box">
+                            <Image
+                                src={PigIcon}
+                                alt={'pig-icon'}
+                                width={80}
+                                height={64}
+                            />
+                        </div>
+                        <div className="text-container">
+                            <div className="text-box">
+                                <p className="title">초보자도 주식 할 수 있다!</p>
+                                <p className="content">주식을 처음할 때 꼭 알아야 하는<br />기초 지식 7가지</p>
+                            </div>
+                        </div>
                     </div>
                 </SwiperSlide>
             </Swiper>
