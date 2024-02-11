aaf3582955f79bc83b27443f86466451b297fb8b
widse <wngud6012@naver.com>
2024-02-11 19:03:06 +0900

    Init(Error) : Fix Build Error && Link Route
    - 30:352  Error: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.  react/no-unescaped-entities

--- src/app/_component/Catergory/TopicList.tsx
+++ src/app/_component/Catergory/TopicList.tsx
@@ -3,6 +3,7 @@
 import style from "./TopicList.module.css";
 import cx from 'classnames';
 import { useState } from "react";
+import Link from "next/link";
 
 function TopicItem() {
     const [heart, setHeart] = useState(false);
@@ -13,18 +14,20 @@ function TopicItem() {
     }
 
     return (
-        <div className={style.item_container}>
-            <div className={style.img_box}></div>
-            <div className={style.content_box}>
-                <p>주식이란?</p>
-                <p>주식이란 주식회사의 자본을 이루는 단위로서의 금액 및 이를 전제로 한 주주의 머시기 머시기</p>
+        <Link href="/stock/1">
+            <div className={style.item_container}>
+                <div className={style.img_box}></div>
+                <div className={style.content_box}>
+                    <p>주식이란?</p>
+                    <p>주식이란 주식회사의 자본을 이루는 단위로서의 금액 및 이를 전제로 한 주주의 머시기 머시기</p>
+                </div>
+                <div className={cx([style.heart_icon_box, heart && style.active])} onClick={handleHeartClick}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" fill="none">
+                    <path stroke="#50BF50" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.458 2.591a5.255 5.255 0 0 0-1.708-1.177 5.122 5.122 0 0 0-4.028 0 5.255 5.255 0 0 0-1.708 1.177L11 3.638 9.986 2.59a5.183 5.183 0 0 0-3.722-1.59 5.183 5.183 0 0 0-3.722 1.59A5.52 5.52 0 0 0 1 6.431a5.52 5.52 0 0 0 1.542 3.841l1.014 1.047L11 19l7.444-7.681 1.014-1.047a5.445 5.445 0 0 0 1.141-1.762 5.58 5.58 0 0 0 0-4.157 5.445 5.445 0 0 0-1.141-1.762Z"/>
+                    </svg>
+                </div>
             </div>
-            <div className={cx([style.heart_icon_box, heart && style.active])} onClick={handleHeartClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" fill="none">
-                <path stroke="#50BF50" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.458 2.591a5.255 5.255 0 0 0-1.708-1.177 5.122 5.122 0 0 0-4.028 0 5.255 5.255 0 0 0-1.708 1.177L11 3.638 9.986 2.59a5.183 5.183 0 0 0-3.722-1.59 5.183 5.183 0 0 0-3.722 1.59A5.52 5.52 0 0 0 1 6.431a5.52 5.52 0 0 0 1.542 3.841l1.014 1.047L11 19l7.444-7.681 1.014-1.047a5.445 5.445 0 0 0 1.141-1.762 5.58 5.58 0 0 0 0-4.157 5.445 5.445 0 0 0-1.141-1.762Z"/>
-                </svg>
-            </div>
-        </div>
+        </Link>
     )
 }
 
--- src/app/_component/Post/UserTypeContent.tsx
+++ src/app/_component/Post/UserTypeContent.tsx
@@ -21,6 +21,8 @@ const mockList = [
 export default function UserTypeContent() {
     const [activeType, setActiveType] = useRecoilState(topicUserType);
 
+    const text = `주식회사는 자본단체이므로 자본이 없이는 성립할 수 없다. 자본은 사원인 주주(株主)의 출자이며, 권리와 의무의 단위로서의 주식으로 나누어진다. 따라서 주식에는 자본을 구성하는 분자로서의 금액의 뜻과, 주주의 회사에 대한 권리·의무의 단위인 주주권(株主權)으로서의 뜻이 있다. 주식과 유사한 것에 합명회사나 합자회사의 지분(持分)이 있으나, 주식은 1인이 많이 소유할 수 있는 데 대하여 지분은 각인의 출자분을 각각 하나의 지분으로 하는 점에 양자의 차이가 있다. 일반적으로 주식과 주권(株券)을 혼동하는 일이 많으나, 주권은 주식(주주권)을 표창하는 유가증권이다. 주식을 줄여 '주'라고 하며, 소유자를 '주주'라고 한다.`
+
     return (
         <div className={style.container}>
             <UserTypeItemList data={mockList} />
@@ -28,7 +30,7 @@ export default function UserTypeContent() {
                 <p className={style.active_name}>{activeType}</p>을 위한 설명
             </div>
             <div className={style.content_text}>
-                주식회사는 자본단체이므로 자본이 없이는 성립할 수 없다. 자본은 사원인 주주(株主)의 출자이며, 권리와 의무의 단위로서의 주식으로 나누어진다. 따라서 주식에는 자본을 구성하는 분자로서의 금액의 뜻과, 주주의 회사에 대한 권리·의무의 단위인 주주권(株主權)으로서의 뜻이 있다. 주식과 유사한 것에 합명회사나 합자회사의 지분(持分)이 있으나, 주식은 1인이 많이 소유할 수 있는 데 대하여 지분은 각인의 출자분을 각각 하나의 지분으로 하는 점에 양자의 차이가 있다. 일반적으로 주식과 주권(株券)을 혼동하는 일이 많으나, 주권은 주식(주주권)을 표창하는 유가증권이다. 주식을 줄여 '주'라고 하며, 소유자를 '주주'라고 한다.
+                {text}
             </div>
         </div>
     )
