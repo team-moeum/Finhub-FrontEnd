9a3bc9badbf888ec5b3288959a6f4ed065410bc5
손주형 <sonjuhyeong@sonjuhyeong-ui-MacBookPro.local>
2024-02-19 00:43:50 +0900

    Init(UI dev): Menu_1

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,10 +1,22 @@
-import Menu from "@/screens/menu";
 import { Metadata } from "next";
+import style from "./menu.module.css";
+import LoginBox from "./_component/LoginBox";
+import MenuCard from "./_component/MenuCard";
+
 
 export const metadata: Metadata = {
   title: "Menu",
 };
 
 export default function MenuPage() {
-  return <Menu />;
+  return (
+    <div className={style.container}>
+      <LoginBox />
+      <div className={style.content}>
+        <MenuCard>공지사항</MenuCard>
+        <MenuCard>앱 설정</MenuCard>
+        <MenuCard>고객센터</MenuCard>
+      </div>
+    </div>
+  );
 }
