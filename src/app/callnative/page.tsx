be334f5ceb121cf2f5109e803bcef59d544fa0ac
chomki32 <chomki32@gmail.com>
2024-04-06 23:09:58 +0900

    test: call jsToNative

--- src/app/callnative/page.tsx
+++ src/app/callnative/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
+import { jsToNative } from '@/utils/jsToNative';
 import style from './callnative.module.css';
+import { useState } from 'react';
 
 export default function CallNative() {
+  const [test, setTest] = useState('');
   return (
       <div className={style.container}>
         <div className={style.content}>
-          <button className={style.test_btn}>Callnative Test</button>
+          <button className={style.test_btn} onClick={() => {
+            jsToNative({val1: 'appVersion', val2: 'b', val3: 'c', val4: 'd'}, (e) => {
+              setTest(e);
+            });
+
+            // setTest('testCall');
+          }}>Callnative Test {test}</button>
         </div>
       </div>
   )
