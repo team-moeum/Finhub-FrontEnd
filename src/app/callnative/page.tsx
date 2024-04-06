5b7bd73718ce8498264453da949636534adbbd48
seungdeok <akk9132@naver.com>
2024-04-07 01:21:07 +0900

     Feat: Test pass event

--- src/app/callnative/page.tsx
+++ src/app/callnative/page.tsx
@@ -1,22 +1,28 @@
 "use client";
 
-import { jsToNative } from '@/utils/jsToNative';
-import style from './callnative.module.css';
-import { useState } from 'react';
+import { jsToNative } from "@/utils/jsToNative";
+import style from "./callnative.module.css";
+import { useState } from "react";
 
 export default function CallNative() {
-  const [test, setTest] = useState('');
+  const [test, setTest] = useState("[]");
   return (
-      <div className={style.container}>
-        <div className={style.content}>
-          <button className={style.test_btn} onClick={() => {
-            jsToNative({val1: 'appVersion', val2: 'b', val3: 'c', val4: 'd'}, (e) => {
-              setTest(e);
-            });
-
-            // setTest('testCall');
-          }}>Callnative Test {test}</button>
-        </div>
+    <div className={style.container}>
+      <div className={style.content}>
+        <button
+          className={style.test_btn}
+          onClick={() => {
+            jsToNative(
+              { val1: "appVersion", val2: "b", val3: "c", val4: "d" },
+              (event: any) => {
+                console.log(event.detail);
+              }
+            );
+          }}
+        >
+          Callnative Test {test}
+        </button>
       </div>
-  )
+    </div>
+  );
 }
