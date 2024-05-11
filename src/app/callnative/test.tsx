9d0bbaf45d6d6a8351d76ce48ebf4786f758fbdf
chomki32 <chomki32@gmail.com>
2024-05-11 22:56:00 +0900

    Feat: example jsToNative #2

---
+++ src/app/callnative/test.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { jsToNative } from "@/utils/jsToNative";
+import style from "./callnative.module.css";
+import { useState } from "react";
+
+export const CallNativeTestPage = () => {
+  const [test, setTest] = useState("[]");
+  return (
+    <div className={style.container}>
+      <div className={style.content}>
+        {test}
+
+        <button
+          className={style.test_btn}
+          onClick={() => {
+            jsToNative(
+              {val1: "appVersion"},
+              (event: any) => {
+                setTest(event.detail);
+              })
+          }}>
+          appVersion
+        </button>
+
+        <button
+          className={style.test_btn}
+          onClick={() => {
+            jsToNative(
+              {val1: "getSafeAreaInset"},
+              (data: any) => {
+                setTest(data.detail);
+              })
+          }}>
+          getSafeAreaInset
+        </button>
+
+        <button
+          className={style.test_btn}
+          onClick={() => {
+            jsToNative(
+              {val1: "share", val2:"share url"},
+              (data: any) => {
+                // Not Used
+              })
+          }}>
+          share
+        </button>
+
+        <button
+          className={style.test_btn}
+          onClick={() => {
+            jsToNative(
+              {val1: "getPushToken"},
+              (data: any) => {
+                setTest(data.detail);
+              })
+          }}>
+          getPushToken
+        </button>
+      </div>
+    </div>
+  );
+}
