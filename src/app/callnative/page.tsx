e22517126b537375b27fd1440474f2e525f1079e
chomki32 <chomki32@gmail.com>
2024-05-11 22:56:00 +0900

    Feat: example jsToNative #2

--- src/app/callnative/page.tsx
+++ src/app/callnative/page.tsx
@@ -1,43 +1,7 @@
-"use client";
-
-import { jsToNative } from "@/utils/jsToNative";
-import style from "./callnative.module.css";
-import { useState } from "react";
+import { CallNativeTestPage } from './test';
 
 export default function CallNative() {
-  const [test, setTest] = useState("[]");
   return (
-    <div className={style.container}>
-      <div className={style.content}>
-        <button
-          className={style.test_btn}
-          onClick={() => {
-            // jsToNative(
-            //   {val1: "appVersion"},
-            //   (event: any) => {
-
-            //   }
-            // );
-
-            // jsToNative(
-            //   {val1: "getSafeAreaInset"},
-            //   (event: any) => {
-
-            //   }
-            // );
-
-            // jsToNative(
-            //   {val1: "share", val2: "share url"},
-            //   (event: any) => {
-
-            //   }
-            // );
-          }}
-        >
-          Callnative Test
-        </button>
-        {test}
-      </div>
-    </div>
-  );
+    <CallNativeTestPage />
+  )
 }
