1e13a45964fd22da63943779021362395f9c67a4
seungdeok <akk9132@naver.com>
2024-05-19 14:11:04 +0900

    feat: add share jstonative

--- src/app/_component/Nav/PostNav.tsx
+++ src/app/_component/Nav/PostNav.tsx
@@ -8,24 +8,28 @@ import BackButton from "./BackButton";
 import { useEffect } from "react";
 
 type Props = {
-  scrap: boolean,
-  topicId: number,
-  onClick: () => void
-}
-export default function PostNav({ scrap, topicId, onClick }: Props) {
-  const scrapIconPath = scrap ? '/icons/scrap_icon_on.svg' : '/icons/scrap_icon_type2.svg';
+  scrap: boolean;
+  topicId: number;
+  onClick: () => void;
+  onShare: () => void;
+};
+export default function PostNav({ scrap, topicId, onClick, onShare }: Props) {
+  const scrapIconPath = scrap
+    ? "/icons/scrap_icon_on.svg"
+    : "/icons/scrap_icon_type2.svg";
 
   return (
     <div className={cx([style.container, style.post])}>
       <BackButton />
       <div className={style.right_box}>
-        <Image 
-          src='/icons/export_icon.svg'
+        <Image
+          src="/icons/export_icon.svg"
           alt="export icon"
           width={18}
           height={18}
+          onClick={onShare}
         />
-        <Image 
+        <Image
           src={scrapIconPath}
           alt="scrap icon"
           width={20}
@@ -34,5 +38,5 @@ export default function PostNav({ scrap, topicId, onClick }: Props) {
         />
       </div>
     </div>
-  )
+  );
 }
