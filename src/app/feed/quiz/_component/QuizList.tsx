8f61e6fb1b8940d98b412c12c771c2a7de138030
widse <wngud6012@naver.com>
2024-06-30 04:04:19 +0900

    refactor: 파일 분리

--- src/app/feed/quiz/_component/QuizList.tsx
+++ src/app/feed/quiz/_component/QuizList.tsx
@@ -11,52 +11,11 @@ import { Box } from '@/components/Box';
 import { FlexBox } from '@/components/FlexBox';
 import { Stack } from '@/components/Stack';
 import { Text } from '@/components/Text';
-import { Button } from '@/components/Button';
 import { useQuizListData } from '../_hooks/useQuizListData';
 import { TodayQuizPopup } from './TodayQuizPopup';
 import { QuizResult } from './QuizResult';
 import { useSolveQuizHook } from '../_hooks/useSolveQuizHook';
-
-import QuizListItemIcon from '@/public/quiz/quiz_icon_list.svg';
-import { LinkButton } from '@/components/LinkButton';
-
-const QuizItem = ({
-  item,
-  onClick
-}: {
-  item: MissedQuiz | SolvedQuiz,
-  onClick?: () => void;
-}) => {
-  return (
-    <Button full height={60} radius={10} backgroundColor='#F9FAFA' onClick={onClick}>
-      <FlexBox width='100%' px={16} gap={12} justifyContent='flex-start'>
-        <QuizListItemIcon />
-        <Stack gap={4}>
-          <Text textAlign='left' size={15} weight={500} color='#494F54'>{item.question}</Text>
-          <Text textAlign='left' size={10} weight={400} color='#CDD1D5'>{item.targetDate}</Text>
-        </Stack>
-      </FlexBox>
-    </Button>
-  )
-}
-
-const NoQuizItem = ({ text }: { text: string }) => {
-  return (
-    <FlexBox width='100%' height={60} radius={10} backgroundColor='#F9FAFA'>
-      <Text size={15} weight={500} color='#7B8287'>{text}</Text>
-    </FlexBox>
-  )
-}
-
-const QuizDetailButton = () => {
-  return (
-    <LinkButton href={`/feed/quiz/detail`}>
-      <FlexBox height={36} backgroundColor="#F6F7F9" radius={10}>
-        <Text size={15} weight={600} color="#A6ABAF">더보기</Text>
-      </FlexBox>
-    </LinkButton>
-  )
-}
+import { NoQuizItem, QuizDetailButton, QuizItem } from './QuizItem';
 
 export const LoginNeedBox = () => {
   return (
--- src/app/feed/quiz/detail/_component/QuizDetailScreen.tsx
+++ src/app/feed/quiz/detail/_component/QuizDetailScreen.tsx
@@ -3,8 +3,27 @@
 import { AppBar } from "@/components/AppBar"
 import { Box } from "@/components/Box"
 import { AppContainer, Container } from "@/components/Container"
+import { Toggle } from "@/components/Toggle"
+import { useState } from "react"
+import { useQuizListData } from "../../_hooks/useQuizListData"
+import moment from "moment"
+
+const SELECTED_TYPE = {
+  missed: 0,
+  solved: 1
+} as const;
 
 export const QuizDetailScreen = () => {
+  const requestDate = moment().format("YYYYMMDD");
+  
+  const [selectedValue, setSelectedValue] = useState<number>(SELECTED_TYPE.missed);
+
+  const {
+    isLoading,
+    missedQuizList,
+    solvedQuizList
+  } = useQuizListData({ requestDate: requestDate });
+
   return (
     <AppContainer header>
       <AppBar
@@ -13,6 +32,15 @@ export const QuizDetailScreen = () => {
       />
 
       <Container mt={22}>
+        <Toggle
+          data={[
+            { text: '놓친 퀴즈', value: SELECTED_TYPE.missed },
+            { text: '풀었던 퀴즈', value: SELECTED_TYPE.solved },
+          ]}
+          selectedValue={selectedValue}
+          onChange={setSelectedValue}
+        />
+
         <Box>Test</Box>
       </Container>
     </AppContainer>
