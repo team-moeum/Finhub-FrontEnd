74b63b31019fe348f8dd1cd61cc9918cf451ba5e
widse <wngud6012@naver.com>
2024-07-05 16:24:41 +0900

    refactor: prefetch Topic Datail

--- src/states/server/List/getTopicInfo.ts
+++ src/states/server/List/getTopicInfo.ts
@@ -1,18 +1,21 @@
-import { fetchApi } from "@/api/fetchApi";
 import { queryKeys } from "../queries";
 import { ApiResponse } from "@/api/type";
+import { get, getSsr } from "@/api/client";
 
-export const getTopicInfo = async(topicId: number, ssr?: boolean) => {
-  const response: ApiResponse = await fetchApi({
-    method: "GET",
-    path: `/api/v1/main/topicInfo?topicId=${topicId}`,
-    tags: queryKeys.topicInfo(topicId),
-    ssr
-  });
+export const getTopicInfo = async(topicId: number) => {
+  const response: ApiResponse = await get(
+    `/api/v1/main/topicInfo?topicId=${topicId}`,
+    queryKeys.topicInfo(topicId),
+  );
 
-  if (response.status === "FAIL") {
-    return [];
-  }
+  return response.data?.topicInfo;
+}
+
+export const getSsrTopicInfo = async(topicId: number) => {
+  const response: ApiResponse = await getSsr(
+    `/api/v1/main/topicInfo?topicId=${topicId}`,
+    queryKeys.topicInfo(topicId),
+  );
 
   return response.data?.topicInfo;
 }
