a60f240bad04389ada80e8a4dfd82f806920349b
widse <wngud6012@naver.com>
2024-08-03 19:29:03 +0900

    FIN-99: 네트워크 오류로 웹이 작동하지 않을 때 어떠한 것을 보여주는지 확인 필요(아무것도 안뜨면 안됨)

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,28 +1,44 @@
 
-'use client' 
+'use client'
 
-import { useEffect } from 'react'
- 
+import Image from "next/image";
+import { AppContainer, Container } from "@/components/Container"
+import { FlexBox } from "@/components/FlexBox"
+import { LinkButton } from "@/components/LinkButton"
+import { Text } from "@/components/Text"
+import { useEffect } from "react"
 
-export default function Error({ error, reset }:{
+export default function Error({ error, reset }: {
   error: Error & { digest?: string }
   reset: () => void
 }) {
   useEffect(() => {
     console.error(error)
   }, [error])
- 
   return (
-    <div>
-      <h2>잘못 됐다.</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        알아봐. {error.message}
-      </button>
-    </div>
+    <AppContainer>
+      <Container>
+        <FlexBox height='calc(100vh - 54px)' direction='column' justifyContent='space-around'>
+          <FlexBox direction='column' gap={10}>
+            <Text size={24} weight={600} color='#7B8287'>오류가 발생했어요.</Text>
+            <Text size={16} weight={600} color='#7B8287'>지속적으로 발생 시 어플 문의 해주세요.</Text>
+          </FlexBox>
+
+          <Image
+            src='/images/error.png'
+            alt='Not Found'
+            width={340}
+            height={224}
+            priority
+          />
+
+          <LinkButton width='100%' href='/'>
+            <FlexBox width='100%' height={55} backgroundColor='#50BF50' radius={12}>
+              <Text size={16} weight={700} color='#FFFFFF'>홈으로</Text>
+            </FlexBox>
+          </LinkButton>
+        </FlexBox>
+      </Container>
+    </AppContainer>
   )
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,18 +6,7 @@ import { AppContainer, Container } from '@/components/Container';
 import { FlexBox } from '@/components/FlexBox';
 import { LinkButton } from '@/components/LinkButton';
 import { Text } from '@/components/Text';
-import styled from '@emotion/styled';
 import Image from 'next/image';
-
-const Container1 = styled.div`
-  padding: 20px 10px;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-`
 
 export default function NotFound() {
   return (
@@ -27,7 +16,7 @@ export default function NotFound() {
           <Text size={24} weight={600} color='#7B8287'>페이지를 찾을 수 없습니다.</Text>
 
           <Image
-            src='/images/not_found_img.png'
+            src='/images/not_found.png'
             alt='Not Found'
             width={340}
             height={224}
