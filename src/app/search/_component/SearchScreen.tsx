ce60003b2216bd8eefc660529ead241f385e905e
widse <wngud6012@naver.com>
2024-09-01 03:32:32 +0900

    FIN-150: 댓글 입력하려고 할 시 키패드 및 커서 요청

--- src/app/search/_component/SearchScreen.tsx
+++ src/app/search/_component/SearchScreen.tsx
@@ -54,7 +54,6 @@ export default function SearchScreen() {
     userInput,
     fetchInput,
     isResultPage,
-    isFocus,
     debouncedOnChangeHandler,
     handleSetInputValue,
     handleInputSubmit,
